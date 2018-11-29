afecf30b9073164e59553ba7ba3cccd092f29eea
whiterabbit-ce <giovannyswam1@hotmail.com>
2018-11-29 13:23:12 +0100

    first Commit

--- app/src/main/java/com/example/retten/retten/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/retten/retten/Activities/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.retten.retten.Activities;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
@@ -14,9 +16,16 @@ import android.widget.Toast;
 
 import com.example.retten.retten.R;
 
+import org.w3c.dom.Text;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    ImageView ImgUserPhoto
+    ImageView ImgUserPhoto;
+             static int PreqCode = 1;
+    static int REQUESTCODE = 1;
+    Uri pickedImgUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +59,50 @@ public class RegisterActivity extends AppCompatActivity {
     private void openGallery(){
         //TODO: open gallery intent and
 
+        Intent galleryIntent =  new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESTCODE);
+
+
     }
 
-    private void checkAndRequestforPermission(){
-            if (ContextCompat.checkSelfPermission( context: RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE )
-        != PackageManager.PERMISSION_GRANTED){
-                if (ActivityCompat.shouldShowRequestPermissionRationale( activity: RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE )){
+    private void checkAndRequestForPermission() {
 
-                Toast.makeText( context: RegisterActivity.this, text: "Please accept for required Permission", Toast.LENGTH_SHORT).show();
-            }
+        if (ContextCompat.checkSelfPermission ( RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(  RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE )){
+
+
+
+            Toast.makeText( context: RegisterActivity.this, resId: "Please Accept Requested Permission", Toast.LENGTH_SHORT).show();
+
         }
+        else
+            {
+
+                ActivityCompat.requestPermissions( RegisterActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PreqCode);
+
+            }
+
+
+            else
+
+
+            {openGallery();}
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode ==REQUESTCODE && data != null)
+            // Bild erfolgreich ausgewählt, Referenz soll gespeichert werden
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
 
     }
 }
