8f5c616baa70d7e48c031a359c97477d52c17125
osamax2 <osobaji@gmail.com>
2018-12-25 18:07:59 +0100

    add picasso

--- app/src/main/java/com/example/retten/retten/Activities/IndividualProduct.java
+++ app/src/main/java/com/example/retten/retten/Activities/IndividualProduct.java
@@ -16,7 +16,7 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.example.retten.retten.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
