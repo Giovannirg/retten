714e438345f752b0406873c76f8404928592caae
osamax2 <osobaji@gmail.com>
2018-12-25 18:21:05 +0100

    fix Cart

--- app/src/main/java/com/example/retten/retten/Activities/ShoppingCartWindow.java
+++ app/src/main/java/com/example/retten/retten/Activities/ShoppingCartWindow.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.retten.retten.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
