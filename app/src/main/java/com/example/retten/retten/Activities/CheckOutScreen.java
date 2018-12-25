d50f6f1171c3beda3d5f8013bfbfb0f5196902ee
whiterabbit-ce <giovannyswam1@hotmail.com>
2018-12-25 18:25:49 +0100

    added layout and java Check out

---
+++ app/src/main/java/com/example/retten/retten/Activities/CheckOutScreen.java
@@ -0,0 +1,18 @@
+package com.example.retten.retten.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.retten.retten.R;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckOutScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out_screen);
+    }
+}
