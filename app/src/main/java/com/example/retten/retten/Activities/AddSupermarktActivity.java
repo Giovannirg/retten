d2598506dda7b2d27244f7676aa61a2afdbd28c6
osamax2 <osobaji@gmail.com>
2019-01-01 17:19:29 +0100

    fix is Admin

---
+++ app/src/main/java/com/example/retten/retten/Activities/AddSupermarktActivity.java
@@ -0,0 +1,13 @@
+package com.example.retten.retten.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSupermarktActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_supermarkt);
+    }
+}
